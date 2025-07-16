b457f343129fa8747f2bd27db785b64cce480b01
Ashwin Naren <arihant2math@gmail.com>
2025-07-16 12:57:01 -0700

    implement idbkeyrange
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- components/shared/net/indexeddb_thread.rs
+++ components/shared/net/indexeddb_thread.rs
@@ -7,6 +7,7 @@ use std::cmp::{PartialEq, PartialOrd};
 use ipc_channel::ipc::IpcSender;
 use serde::{Deserialize, Serialize};
 use servo_url::origin::ImmutableOrigin;
+use malloc_size_of_derive::MallocSizeOf;
 
 // std::error::Error implemented debug, could be useful for displaying the error
 pub type DbError = Box<dyn std::error::Error + Send + Sync>;
@@ -21,7 +22,7 @@ pub enum IndexedDBTxnMode {
 }
 
 /// <https://www.w3.org/TR/IndexedDB-2/#key-type>
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, MallocSizeOf, Serialize)]
 pub enum IndexedDBKeyType {
     Number(f64),
     String(String),
@@ -118,7 +119,7 @@ impl PartialEq for IndexedDBKeyType {
 }
 
 // <https://www.w3.org/TR/IndexedDB-2/#key-range>
-#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[derive(Clone, Debug, Default, Deserialize, MallocSizeOf, Serialize)]
 #[allow(unused)]
 pub struct IndexedDBKeyRange {
     pub lower: Option<IndexedDBKeyType>,
@@ -138,6 +139,37 @@ impl From<IndexedDBKeyType> for IndexedDBKeyRange {
 }
 
 impl IndexedDBKeyRange {
+    pub fn only(key: IndexedDBKeyType) -> Self {
+        Self::from(key)
+    }
+
+    pub fn new(lower: Option<IndexedDBKeyType>, upper: Option<IndexedDBKeyType>, lower_open: bool, upper_open: bool) -> Self {
+        IndexedDBKeyRange {
+            lower,
+            upper,
+            lower_open,
+            upper_open,
+        }
+    }
+
+    pub fn lower_bound(key: IndexedDBKeyType, open: bool) -> Self {
+        IndexedDBKeyRange {
+            lower: Some(key),
+            upper: None,
+            lower_open: open,
+            upper_open: false,
+        }
+    }
+
+    pub fn upper_bound(key: IndexedDBKeyType, open: bool) -> Self {
+        IndexedDBKeyRange {
+            lower: None,
+            upper: Some(key),
+            lower_open: false,
+            upper_open: open,
+        }
+    }
+
     // <https://www.w3.org/TR/IndexedDB-2/#in>
     pub fn contains(&self, key: &IndexedDBKeyType) -> bool {
         // A key is in a key range if both of the following conditions are fulfilled:
